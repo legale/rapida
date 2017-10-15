ca18b6ce438c6b944a8d0c6fadf2a84bc03b9936
legale <legale.legale@gmail.com>
2017-10-15 12:56:31 +0200

    rapida 0.0.5.2

--- api/Config.php
+++ api/Config.php
@@ -15,7 +15,7 @@ require_once('Simpla.php');
 
 class Config
 {
-	public $version = '0.0.5.1';
+	public $version = '0.0.5.2';
 	
 	// Файл для хранения настроек
 	public $config_file = 'config/config.ini';
