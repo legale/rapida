75d0e0a165e8bf8f05690d3afe53991ea3f84b74
johovich <johovich@yandex.ru>
2018-01-31 20:42:56 +0300

    add_product() switched to translit_ya()
    Former-commit-id: 63f1aec1536b1eb9fbc04d1189526fc9ea04967e
    Former-commit-id: 89ced6d7a6692716f5cd0f9327ee8d2dbb1b34e5

--- view/View.php
+++ view/View.php
@@ -76,12 +76,12 @@ class View extends Simpla
 
 
 			// Текущая страница (если есть)
-			$subdir = substr(dirname(dirname(__FILE__)), strlen($_SERVER['DOCUMENT_ROOT']));
-			$page_url = trim(substr($_SERVER['REQUEST_URI'], strlen($subdir)),"/");
-			if(strpos($page_url, '?') !== false)
-				$page_url = substr($page_url, 0, strpos($page_url, '?'));
-			$this->page = $this->pages->get_page((string)$page_url);
-			$this->design->assign('page', $this->page);		
+			//~ $subdir = substr(dirname(dirname(__FILE__)), strlen($_SERVER['DOCUMENT_ROOT']));
+			//~ $page_url = trim(substr($_SERVER['REQUEST_URI'], strlen($subdir)),"/");
+			//~ if(strpos($page_url, '?') !== false)
+				//~ $page_url = substr($page_url, 0, strpos($page_url, '?'));
+			//~ $this->page = $this->pages->get_page((string)$page_url);
+			//~ $this->design->assign('page', $this->page);		
 			
 			// Передаем в дизайн то, что может понадобиться в нем
 			$this->design->assign('currencies',	$this->currencies);
