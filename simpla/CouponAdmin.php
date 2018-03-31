53b99ccac82a53635d7ea52c820871a4bafbf736
legale <legale.legale@gmail.com>
2018-03-31 23:33:38 +0200

    0.0.9b3
    Former-commit-id: 78d162aa6029bb7f9f100595348d90fe665990f1

--- simpla/CouponAdmin.php
+++ simpla/CouponAdmin.php
@@ -5,35 +5,35 @@ class CouponAdmin extends Simpla
 {
 	public function fetch()
 	{
-		$coupon = new stdClass;
+		$coupon = array();
 		if ($this->request->method('post'))
 			{
-			$coupon->id = $this->request->post('id', 'integer');
-			$coupon->code = $this->request->post('code', 'string');
+			$coupon['id'] = $this->request->post('id', 'integer');
+			$coupon['code'] = $this->request->post('code', 'string');
 			if ($this->request->post('expires'))
-				$coupon->expire = date('Y-m-d', strtotime($this->request->post('expire')));
+				$coupon['expire'] = date('Y-m-d', strtotime($this->request->post('expire')));
 			else
-				$coupon->expire = null;
-			$coupon->value = $this->request->post('value', 'float');
-			$coupon->type = $this->request->post('type', 'string');
-			$coupon->min_order_price = $this->request->post('min_order_price', 'float');
-			$coupon->single = $this->request->post('single', 'float');
+				$coupon['expire'] = null;
+			$coupon['value'] = $this->request->post('value', 'float');
+			$coupon['type'] = $this->request->post('type', 'string');
+			$coupon['min_order_price'] = $this->request->post('min_order_price', 'float');
+			$coupon['single'] = $this->request->post('single', 'float');
 
  			// Не допустить одинаковые URL разделов.
-			if ( ($a = $this->coupons->get_coupon((string)$coupon->code)) && $a->id != $coupon->id)
+			if ( ($a = $this->coupons->get_coupon((string)$coupon['code'])) && $a->id != $coupon['id'])
 				{
 				$this->design->assign('message_error', 'code_exists');
 			}
-			elseif (empty($coupon->code))
+			elseif (empty($coupon['code']))
 				{
 				$this->design->assign('message_error', 'code_empty');
 			}
 			else
 				{
-				if (empty($coupon->id))
+				if (empty($coupon['id']))
 					{
-					if ($coupon->id = $this->coupons->add_coupon($coupon)) {
-						$coupon = $this->coupons->get_coupon($coupon->id);
+					if ($coupon['id'] = $this->coupons->add_coupon($coupon)) {
+						$coupon = $this->coupons->get_coupon($coupon['id']);
 						$this->design->assign('message_success', 'added');
 					}
 					else {
@@ -42,8 +42,8 @@ class CouponAdmin extends Simpla
 				}
 				else
 					{
-					$this->coupons->update_coupon($coupon->id, $coupon);
-					$coupon = $this->coupons->get_coupon($coupon->id);
+					$this->coupons->update_coupon($coupon['id'], $coupon);
+					$coupon = $this->coupons->get_coupon($coupon['id']);
 					$this->design->assign('message_success', 'updated');
 				}
 
@@ -52,12 +52,12 @@ class CouponAdmin extends Simpla
 		}
 		else
 			{
-			$coupon->id = $this->request->get('id', 'integer');
-			$coupon = $this->coupons->get_coupon($coupon->id);
+			$coupon['id'] = $this->request->get('id', 'integer');
+			$coupon = $this->coupons->get_coupon($coupon['id']);
 		}
 
-//		if(empty($coupon->id))
-//			$coupon->expire = date($this->settings->date_format, time());
+//		if(empty($coupon['id']))
+//			$coupon['expire'] = date($this->settings->date_format, time());
 
 		$this->design->assign('coupon', $coupon);
 		return $this->design->fetch('coupon.tpl');
