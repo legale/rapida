98b917a25ad40a4ed60d1fd98b206fdd920cef9a
legale <legale.legale@gmail.com>
2018-04-04 00:35:39 +0200

    0.0.9b4
    Former-commit-id: 49f74778183889534252591ed4552ec9f5f83c9a
    Former-commit-id: 5ecbdf5be1ae563b428571a6be6d1ba6e8038065

--- api/ControllerSimpla.php
+++ api/ControllerSimpla.php
@@ -4,49 +4,44 @@
  * Это класс контроллера для перехода во внутренности Simpla. Этакий мост
  */
 
-require_once ('Simpla.php');
+require_once('Simpla.php');
 
 class ControllerSimpla extends Simpla
 {
 
-	public function __construct()
-	{
-		dtimer::log(__METHOD__ . ' constructor');
-	}
+    public function __construct()
+    {
+        dtimer::log(__METHOD__ . ' constructor');
+    }
 
-	public function action($url = null)
-	{
+    public function action($url = null)
+    {
 
-		require_once ('view/IndexView.php');
-		$view = new IndexView();
+        require_once('view/IndexView.php');
+        $view = new IndexView();
 
-		if($url === '404'){
-			print $view->fetch('404');
-			return true;
-		}
+        if ($url === '404') {
+            print $view->fetch('404');
+            return true;
+        }
 
 
+        // Если все хорошо
+        if (($res = $view->fetch()) !== false) {
+            // Выводим результат
+            header("Content-type: text/html; charset=UTF-8");
+            print $res;
 
-		// Если все хорошо
-		if ( ($res = $view->fetch()) !== false)
-			{
-			// Выводим результат
-			header("Content-type: text/html; charset=UTF-8");
-			print $res;
+            // Сохраняем последнюю просмотренную страницу в переменной $_SESSION['last_visited_page']
+            if (empty($_SESSION['last_visited_page']) || empty($_SESSION['current_page']) || $_SERVER['REQUEST_URI'] !== $_SESSION['current_page']) {
+                if (!empty($_SESSION['current_page']) && !empty($_SESSION['last_visited_page']) && $_SESSION['last_visited_page'] !== $_SESSION['current_page'])
+                    $_SESSION['last_visited_page'] = $_SESSION['current_page'];
+                $_SESSION['current_page'] = $_SERVER['REQUEST_URI'];
+            }
+        } else {
+            print $view->fetch('404');
+        }
 
-			// Сохраняем последнюю просмотренную страницу в переменной $_SESSION['last_visited_page']
-			if (empty($_SESSION['last_visited_page']) || empty($_SESSION['current_page']) || $_SERVER['REQUEST_URI'] !== $_SESSION['current_page'])
-				{
-				if (!empty($_SESSION['current_page']) && !empty($_SESSION['last_visited_page']) && $_SESSION['last_visited_page'] !== $_SESSION['current_page'])
-					$_SESSION['last_visited_page'] = $_SESSION['current_page'];
-				$_SESSION['current_page'] = $_SERVER['REQUEST_URI'];
-			}
-		}
-		else
-			{ 
-			print $view->fetch('404');
-		}
-
-	}
+    }
 
 }
--- view/PageView.php
+++ view/PageView.php
@@ -8,24 +8,28 @@ require_once('View.php');
 
 class PageView extends View
 {
-	function fetch($url = null)
-	{
-		if(!isset($url)){
-			$url = $this->root->uri_arr['path']['url'];
-		}
-		$page = $this->pages->get_page($url);
-		
-		// Отображать скрытые страницы только админу, иначе 404
-		if(empty($page) || (!$page['visible'] && empty($_SESSION['admin']))){
-			header("http/1.0 404 not found");
-			$page = $this->pages->get_page('404');
-		}
-		
-		$this->design->assign('page', $page);
-		$this->design->assign('meta_title', $page['meta_title']);
-		$this->design->assign('meta_keywords', $page['meta_keywords']);
-		$this->design->assign('meta_description', $page['meta_description']);
-		
-		return $this->design->fetch('page.tpl');
-	}
+    function fetch($url = null)
+    {
+        dtimer::log(__METHOD__. " start url: ".var_export($url,true));
+        if (!isset($url)) {
+            $url = $this->root->uri_arr['path']['url'];
+        }
+        $page = $this->pages->get_page($url);
+
+        // Отображать скрытые страницы только админу, иначе 404
+        if (empty($page) || (!$page['visible'] && empty($_SESSION['admin']))) {
+            $url = '404';
+            $page = $this->pages->get_page($url);
+        }
+
+        if($url === '404'){
+            header("http/1.0 404 not found");
+        }
+        $this->design->assign('page', $page);
+        $this->design->assign('meta_title', $page['meta_title']);
+        $this->design->assign('meta_keywords', $page['meta_keywords']);
+        $this->design->assign('meta_description', $page['meta_description']);
+
+        return $this->design->fetch('page.tpl');
+    }
 }
