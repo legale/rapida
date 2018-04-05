173178be6cf88ee75fd92c84cff6c90bab2a6d9c
legale <legale.legale@gmail.com>
2018-04-06 01:25:06 +0200

    0.0.9b4
    Former-commit-id: f19760eda3583e43550823a3e72ca0d89cd6b7f1 [formerly 7e1e2c818cfcfa91b3edbbdeb61952f50d75d200]
    Former-commit-id: 2b17243f7b16f5d3d3a2278f2ef0824109557b89

--- simpla/ajax/search_products.php
+++ simpla/ajax/search_products.php
@@ -1,41 +1,39 @@
 <?php
-	require_once('../../api/Simpla.php');
-	$simpla = new Simpla();
-	$limit = 100;
-	
-	$keyword = $simpla->request->get('query', 'string');
-	
-	$keywords = explode(' ', $keyword);
-	$keyword_sql = '';
-	foreach($keywords as $keyword)
-	{
-				$kw = $simpla->db->escape(trim($keyword));
-				$keyword_sql .= $simpla->db->placehold("AND (p.name LIKE '%$kw%' OR p.meta_keywords LIKE '%$kw%' OR p.id in (SELECT product_id FROM __variants WHERE sku LIKE '%$kw%'))");
-	}
-	
-	
-	$simpla->db->query('SELECT p.id, p.name, i.filename as image FROM __products p
-	                    LEFT JOIN __images i ON i.product_id=p.id AND i.pos=(SELECT MIN(pos) FROM __images WHERE product_id=p.id LIMIT 1)
-	                    WHERE 1 '.$keyword_sql.' ORDER BY p.name LIMIT ?', $limit);
-	$products = $simpla->db->results_array();
+require_once('../../api/Simpla.php');
+$simpla = new Simpla();
+$limit = 100;
 
-	$suggestions = array();
-	foreach($products as $product)
-	{
-		if(!empty($product['image']))
-			$product['image'] = $simpla->design->resize_modifier($product['image'], 35, 35);
-		
-		$suggestion = new stdClass();
-		$suggestion->value = $product['name'];
-		$suggestion->data = $product;
-		$suggestions[] = $suggestion;
-	}
-	
-	$res = new stdClass;
-	$res->query = $keyword;
-	$res->suggestions = $suggestions;
-	header("Content-type: application/json; charset=UTF-8");
-	header("Cache-Control: must-revalidate");
-	header("Pragma: no-cache");
-	header("Expires: -1");		
-	print json_encode($res);
+$keyword = $simpla->request->get('query', 'string');
+
+$keywords = explode(' ', $keyword);
+$keyword_sql = '';
+foreach ($keywords as $keyword) {
+    $kw = $simpla->db->escape(trim($keyword));
+    $keyword_sql .= $simpla->db->placehold("AND (p.name LIKE '%$kw%' OR p.meta_keywords LIKE '%$kw%' OR p.id in (SELECT product_id FROM __variants WHERE sku LIKE '%$kw%'))");
+}
+
+
+$simpla->db->query('SELECT p.id, p.name, p.image, p.image_id FROM __products p WHERE 1 ' . $keyword_sql . ' ORDER BY p.name LIMIT ?', $limit);
+$products = $simpla->db->results_array();
+
+$suggestions = array();
+if (is_array($products)) {
+    foreach ($products as $product) {
+        if (!empty($product['image'])) {
+            $product['image'] = $simpla->design->resize_modifier($product['image'], 'products', $product['image_id'], 35, 35);
+        }
+        $suggestion = new stdClass();
+        $suggestion->value = $product['name'];
+        $suggestion->data = $product;
+        $suggestions[] = $suggestion;
+    }
+}
+
+$res = new stdClass;
+$res->query = $keyword;
+$res->suggestions = $suggestions;
+header("Content-type: application/json; charset=UTF-8");
+header("Cache-Control: must-revalidate");
+header("Pragma: no-cache");
+header("Expires: -1");
+print json_encode($res);
