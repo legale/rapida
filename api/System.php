f25392423fdc4ad59bce1908a348f0396a30f72b
legale <legale.legale@gmail.com>
2018-04-28 09:19:40 +0300

    0.0.9b9
    Former-commit-id: 903f38c74f00449e5bfc97b23dff49c7ce144916
    Former-commit-id: 42147cd3579fe373e469205b5ea1cf606253758a

--- api/System.php
+++ api/System.php
@@ -1,139 +1,133 @@
 <?php
-require_once ('Simpla.php');
+require_once('Simpla.php');
 
 class System extends Simpla
 {
 
-	/*
-	 * Это метод для синхронизации таблицы features и options
-	 * Удаляет лишние и добавляет недостающие столбцы в таблицу options на основе id из таблицы features 
-	 */
-	public function sync_options()
-	{
-		$fids = array();
-		$cols = array();
-		
-		//получим нужные нам id свойств из features
-		if ($this->db->query("SELECT id FROM __features")) {
-			$fids = $this->db->results_array('id');
-		}
-		else {
-			return false;
-		}
-		//получим столбцы из таблицы options
-		if ($this->db->query("SHOW COLUMNS FROM __options")) {
-			if ($cols = $this->db->results_array('Field')) {
-				$cols = array_combine($cols, $cols);
-			}
-		}
-		else {
-			return false;
-		}
-		
-		//Уберем product_id из массива - это поле проверять не нужно
-		if (isset($cols['product_id'])) {
-			unset($cols['product_id']);
-		}
-		else {
-			return false;
-		}
-		
-		//проверим все свойства на предмет их наличия в таблице options
-		if (is_array($fids)) {
-			foreach ($fids as $fid) {
-				//Если столбца нет - добавим его
-				if (!array_key_exists($fid, $cols)) {
-					$this->db->query("ALTER TABLE __options ADD `$fid` MEDIUMINT NULL");
-				}
-				else {
-				//если он есть, уберем его из массива $cols
-					unset($cols[$fid]);
-				}
-			}
-		}
-		//Если у нас что-то осталось в массиве $cols - надо удалить это оттуда
-		if (count($cols) > 0) {
-			foreach ($cols as $col) {
-				$this->db->query("ALTER TABLE __options DROP `$col`");
-			}
-		}
+    /*
+     * Это метод для синхронизации таблицы features и options
+     * Удаляет лишние и добавляет недостающие столбцы в таблицу options на основе id из таблицы features
+     */
+    public function sync_options()
+    {
+        $fids = array();
+        $cols = array();
 
-		return true;
+        //получим нужные нам id свойств из features
+        if ($this->db->query("SELECT id FROM __features")) {
+            $fids = $this->db->results_array('id');
+        } else {
+            return false;
+        }
+        //получим столбцы из таблицы options
+        if ($this->db->query("SHOW COLUMNS FROM __options")) {
+            if ($cols = $this->db->results_array('Field')) {
+                $cols = array_combine($cols, $cols);
+            }
+        } else {
+            return false;
+        }
 
-	}
+        //Уберем product_id из массива - это поле проверять не нужно
+        if (isset($cols['product_id'])) {
+            unset($cols['product_id']);
+        } else {
+            return false;
+        }
 
-	/*
-	 * Данный метод предназначен для очистки таблицы options_uniq от неиспользуемых в товарах свойствах.
-	 */
+        //проверим все свойства на предмет их наличия в таблице options
+        if (is_array($fids)) {
+            foreach ($fids as $fid) {
+                //Если столбца нет - добавим его
+                if (!array_key_exists($fid, $cols)) {
+                    $this->db->query("ALTER TABLE __options ADD `$fid` MEDIUMINT NULL");
+                } else {
+                    //если он есть, уберем его из массива $cols
+                    unset($cols[$fid]);
+                }
+            }
+        }
+        //Если у нас что-то осталось в массиве $cols - надо удалить это оттуда
+        if (count($cols) > 0) {
+            foreach ($cols as $col) {
+                $this->db->query("ALTER TABLE __options DROP `$col`");
+            }
+        }
 
-	public function clear_options()
-	{
-		//сначала надо вытащить id всех уникальных опции из таблицы options_uniq
+        return true;
 
-		if ($this->db->query("SELECT `id` FROM __options_uniq")) {
-			
-			//так мы получим value ids $vids в виде массива с ключами в виде id значений свойств
-			$vids = $this->db->results_array(null, 'id', true);
-			
-			//Теперь будем проверять каждую ячейку таблицы options на предмет наличия там id из $vids.
-			//Если будет совпадение, то мы удалим значение из массива $vids. Оставшиеся в массиве id и будут лишними.
+    }
 
-			if ($this->db->query("SELECT * FROM __options")) {
-				while ($row = $this->db->result_array(null, 'product_id', true)) {
-					foreach ($row as $vid) {
-						if (array_key_exists($vid, $vids)) {
-							unset($vids[$vid]);
-						}
-					}
-				}
-			}
-			
-			//теперь удалим лишнее
-			if ($vids !== false && count($vids) > 0) {
-				foreach ($vids as $vid => $v) {
-					$this->db->query("DELETE `o` FROM __options_uniq AS `o` WHERE `id` = '$vid'");
-				}
-			}
-			//Если удалось дойти до конца вернем true
-			return true;
+    /*
+     * Данный метод предназначен для очистки таблицы options_uniq от неиспользуемых в товарах свойствах.
+     */
 
-		}
-		else {
-			return false;
-		}
+    public function clear_options()
+    {
+        //сначала надо вытащить id всех уникальных опции из таблицы options_uniq
+
+        if ($this->db->query("SELECT `id` FROM __options_uniq")) {
+
+            //так мы получим value ids $vids в виде массива с ключами в виде id значений свойств
+            $vids = $this->db->results_array(null, 'id', true);
+
+            //Теперь будем проверять каждую ячейку таблицы options на предмет наличия там id из $vids.
+            //Если будет совпадение, то мы удалим значение из массива $vids. Оставшиеся в массиве id и будут лишними.
+
+            if ($this->db->query("SELECT * FROM __options")) {
+                while ($row = $this->db->result_array(null, 'product_id', true)) {
+                    foreach ($row as $vid) {
+                        if (array_key_exists($vid, $vids)) {
+                            unset($vids[$vid]);
+                        }
+                    }
+                }
+            }
+
+            //теперь удалим лишнее
+            if ($vids !== false && count($vids) > 0) {
+                foreach ($vids as $vid => $v) {
+                    $this->db->query("DELETE `o` FROM __options_uniq AS `o` WHERE `id` = '$vid'");
+                }
+            }
+            //Если удалось дойти до конца вернем true
+            return true;
+
+        } else {
+            return false;
+        }
 
 
-	}
+    }
 
-	/*
-	 * Данный метод предназначен для очистки таблицы options_uniq от неиспользуемых в товарах свойствах.
-	 */
+    /*
+     * Данный метод предназначен для очистки таблицы options_uniq от неиспользуемых в товарах свойствах.
+     */
 
-	public function download_all_images()
-	{
-		$q = "SELECT id 
+    public function download_all_images()
+    {
+        $q = "SELECT id 
 		FROM __img_products 
 		WHERE 1
 		AND `basename` LIKE 'http:%' 
 		OR `basename` LIKE 'https:%'";
-		
-		//получаем список подходящих файлов 
-		if ($this->db->query($q)) {
-			$ids = $this->db->results_array('id');
-			//перебираем список и скачиваем эти файлы
-			if ($ids) {
-				foreach ($ids as $id) {
-					$task = '$this->image->download("products",'.$id.');';
-					$keyhash = hash('md5', $task);
-					$this->queue->addtask($keyhash, __METHOD__ , $task);
-				}
-			}
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
+
+        //получаем список подходящих файлов
+        if ($this->db->query($q)) {
+            $ids = $this->db->results_array('id');
+            //перебираем список и скачиваем эти файлы
+            if ($ids) {
+                foreach ($ids as $id) {
+                    $task = '$this->image->download("products",' . $id . ');';
+                    $keyhash = md5($task);
+                    $this->queue->addtask($keyhash, __METHOD__, $task);
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 
 }
