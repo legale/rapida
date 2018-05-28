31904f86b712aa5bfe051f4a25efea1a981ca0b4
legale <legale.legale@gmail.com>
2018-05-28 21:48:32 +0200

    0.0.10b23

--- view/ProductsView.php
+++ view/ProductsView.php
@@ -186,7 +186,7 @@ class ProductsView extends View
                         continue;
                     }
                     $vals_string = implode(', ', $vals_text);
-                    $meta_filter[] = $this->design->morpher_modifier($features[$fid]['name'] . " " . $vals_string, 'r', true);
+                    $meta_filter[] = $this->design->morpher_modifier($features[$fid]['name'] . " " . $vals_string, 'r');
                 }
             }
         }
