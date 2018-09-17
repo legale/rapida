dcb21e4f710f58fbf1406f50bfc5b091757de649
ru <ru@ru-mi.Home>
2018-09-17 07:00:49 +0200

    0.0.14b6

--- cron/sitemap.php
+++ cron/sitemap.php
@@ -329,9 +329,7 @@ function &categories_features2_gen(array &$params, &$rapida): array
                 $part2 = $features[$fid2]['trans'] . "-" . $options['full'][$fid2]['trans'][$vid2];
                 $url = HOSTNAME . 'catalog/' . $c['trans'] . "/" . $part1 . "/" . $part2;
 
-//                echo $url .PHP_EOL;
-//                dtimer::show_console(100);
-//                die;
+
 
                 fwrite($params['fopen'], gen_url_string($url));
                 ++$params['counter'];
@@ -416,10 +414,7 @@ function &categories_brands_features_gen(array &$params, &$rapida): array
                     //это для подсчета страниц пагинации (не используется сейчас)
                     //$pages = (int)ceil($p_count / ITEMS);
                     if ($p_count) {
-                        if(isset($features[$fid])){
-                            print_r($options['filter']);
-                            die;
-                        }
+
                         $part2 = $features[$fid]['trans'] . "-" . $options['full'][$fid]['trans'][$vid];
                         $url = HOSTNAME . 'catalog/' . $c['trans'] . "/" . $part1 . "/" . $part2;
                         fwrite($params['fopen'], gen_url_string($url));
