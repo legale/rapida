23b5d6c7790f6ccf0b32ca460be5c48b57c42e74
ru <ru@ru-mi.Home>
2018-09-27 17:02:08 +0200

    0.0.15b2

--- view/ProductsView.php
+++ view/ProductsView.php
@@ -479,6 +479,9 @@ class ProductsView extends View
 
             //тут проверим количество переданных значений опций и количество полученных из базы,
             //если не совпадает - return false
+            if (!is_iterable($ids)) {
+                dtimer::log(" ids is not countable. " . var_export($ids, true), 2);
+            }
             if ($ids === false || count($ids) !== count($vals)) {
                 dtimer::log(__METHOD__ . ' given names and founded ids not equal. return false ' . var_export($ids, true) . var_export($vals, true), 2);
                 //запускаем снова, если это был первый запуск
